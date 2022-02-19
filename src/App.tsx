32634929b33ac7fee5ecdfbd37a9749d9e69d7b4
galaxyjiang <876708058@qq.com>
2022-02-19 20:13:05 +0800

    创建方块

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 import {Side} from "./components/Side";
 
 function App() {
--- src/components/Side.tsx
+++ src/components/Side.tsx
@@ -1,33 +1,56 @@
 import React, {useState} from "react";
-import '../App.css';
+import '../App.scss';
 
 export function Side() {
-    let row = 3;
-    let column = 2;
+    let row = 30;
+    let column = 20;
     let matrix = [];
-    let initLine = [];
+
     let square = [[1, 1], [1, 1]];
-    for (let i = 0; i < column; i++) {
-        initLine.push(0);
-    }
+
     for (let i = 0; i < row; i++) {
+        let initLine = [];
+        for (let i = 0; i < column; i++) {
+            initLine.push(0);
+        }
         matrix.push(initLine);
     }
     const [initmatrix, setInitmatrix] = useState(matrix);
+
+    function createSquare() {
+
+        square.map((item, index) => {
+            for(let i =0;i<item.length;i++){
+                initmatrix[index][i] = 1
+                }
+
+        });
+        return initmatrix;
+    }
+    createSquare()
+
     return (
         <div className="Side">
             <h1>俄罗斯方块</h1>
             <button>开始游戏</button>
             <div className="blank">
                 {
+
                     initmatrix.map((item) => {
+
                             return <div className="row">{item.map(item => {
-                                return <div className="col"/>;
+                                if(item === 0){
+                                    return <div className="col"/>;
+                                }
+                                if(item === 1){
+                                    return <div className="col active"/>;
+                                }
                             })}</div>;
                         }
                     )
                 }
             </div>
+
 
         </div>
     );
