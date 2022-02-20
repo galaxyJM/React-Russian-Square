b02789941e6775cef5ee4a61d1665da6efbba963
galaxyjiang <876708058@qq.com>
2022-02-20 19:04:41 +0800

    让方块自动下落

--- src/components/Matrix.tsx
+++ src/components/Matrix.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import '../App.scss';
 import {square} from "../const/square";
 
@@ -6,38 +6,65 @@ type Props = {
     matrix: number[][]
 }
 
-export function Matrix(props:Props) {
-    let blankMatrix = props.matrix
+export function Matrix(props: Props) {
+    let blankMatrix:number[][] = deepClone(props.matrix);
     const [matrix, setMatrix] = useState(blankMatrix);
-    function createSquare() {
-        square["S"].map((item, index) => {
-                for (let i = 0; i < item.length; i++) {
-                    matrix[index][i] = item[i];
-                }
-                return undefined
+    function deepClone(aim:number[][]){
+        let res = []
+        for (let i=0;i<aim.length;i++){
+            res.push([...aim[i]])
         }
-        );
-        setMatrix([...matrix])
+        return res
     }
+    function createSquare(offset: number) {
+        square["S"].map((item, index) => {
+
+                setMatrix((matrix)=>{
+                    for (let i = 0; i < item.length; i++) {
+                        matrix[index + offset][i] = item[i];
+                    }
+                    return deepClone(matrix)
+                })
+                return undefined;
+            }
+        );
+    }
+    function autoDown() {
+        let offset = 0;
+        createSquare(offset);
+        offset++
+        const timer = setInterval(() => {
+            setMatrix(deepClone(props.matrix))
+            createSquare(offset);
+            offset++;
+            if (offset === blankMatrix.length-1) {
+                clearInterval(timer);
+            }
+        }, 1000);
+    }
+    useEffect(()=>{
+        console.log(matrix);
+    })
+
 
 
 
     return (
         <div className="Side">
             <h1>俄罗斯方块</h1>
-            <button onClick={() => createSquare()}>开始游戏</button>
+            <button onClick={() => autoDown()}>开始游戏</button>
             <div className="blank">
                 {
-                    matrix.map((item,index) => {
+                    matrix.map((item, index) => {
 
-                            return <div className="row" key={index}>{item.map((item,index) => {
+                            return <div className="row" key={index}>{item.map((item, index) => {
                                 if (item === 0) {
                                     return <div className="col" key={index}/>;
                                 }
                                 if (item === 1) {
                                     return <div className="col active" key={index}/>;
-                                }else{
-                                    return undefined
+                                } else {
+                                    return undefined;
                                 }
                             })}</div>;
                         }
