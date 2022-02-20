d93eccfe3f1102e485e3e8146cb57a823ade74ce
galaxyjiang <876708058@qq.com>
2022-02-20 19:04:59 +0800

    让方块自动下落

---
+++ src/const/square.ts
@@ -0,0 +1,21 @@
+const square = {
+    O: [[1, 1], [1, 1]],
+    I: [[1], [1], [1], [1]],
+    S: [[0, 1, 1], [1, 1, 0]],
+    Z: [[1, 1, 0], [0, 1, 1]],
+    L: [[1, 0], [1, 0], [1, 1]],
+    J: [[0, 1], [0, 1], [1, 1]],
+    T: [[1, 1, 1], [0, 1, 0]]
+};
+
+const row = 30;
+const column = 20;
+let matrix:number[][] = [];
+for (let i = 0; i < row; i++) {
+    let initLine = [];
+    for (let i = 0; i < column; i++) {
+        initLine.push(0);
+    }
+    matrix.push(initLine);
+}
+export {matrix, square};
